9906b8997199ec61d7ef8764c1c17ca359481145
jeroen jongkees <jeroenjongkees@gmail.com>
2023-05-21 21:27:39 +0200

    Spring Data JDBC applied

--- taco-cloud/src/main/java/tacos/Taco.java
+++ taco-cloud/src/main/java/tacos/Taco.java
@@ -6,24 +6,29 @@ import java.util.List;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
 import lombok.Data;
 
 @Data
+@Table
 public class Taco {
 
-  private Long id;
+	@Id
+	private Long id;
 
-  private Date createdAt = new Date();
+	private Date createdAt = new Date();
 
-  @NotNull
-  @Size(min=5, message="Name must be at least 5 characters long")
-  private String name;
+	@NotNull
+	@Size(min=5, message="Name must be at least 5 characters long")
+	private String name;
 
-  @Size(min=1, message="You must choose at least 1 ingredient")
-  private List<IngredientRef> ingredients = new ArrayList<>();
+	@Size(min=1, message="You must choose at least 1 ingredient")
+	private List<IngredientRef> ingredients = new ArrayList<>();
 
-  public void addIngredient(Ingredient taco) {
-    this.ingredients.add(new IngredientRef(taco.getId()));
-  }
+	public void addIngredient(Ingredient taco) {
+		this.ingredients.add(new IngredientRef(taco.getId()));
+	}
 
 }
